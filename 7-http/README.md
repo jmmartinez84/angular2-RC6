763b4b46c784b2fba6bd64725820065eeaf8484b
Alberto Basalo <albertobasalo@hotmail.com>
2016-09-13 17:59:35 +0200

    7 ok

--- 7-http/README.md
+++ 7-http/README.md
@@ -4,4 +4,6 @@ Demostración de comunicaciones http básicas con Angular 2
 
 
 ### Guía
-
+- `home.module.ts` : importación y registro del módulo HttpModule
+- `movimientos.service.ts` : refactoring para consumir un servicio REST
+- `movimiento.component.ts` : refactoring para subscribirse a un observable
--- 7-http/src/app/home/home.module.ts
+++ 7-http/src/app/home/home.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from "@angular/common";
 import { FormsModule }   from '@angular/forms';
 import { RouterModule } from '@angular/router';
-/** 1. importación del módulo de comunicaciones */
+/** 
+ * 1. importación del módulo de comunicaciones
+ * */
 import {HttpModule} from "@angular/http";
 
 import { MovimientoComponent }   from './movimiento/movimiento.component';
@@ -12,7 +14,9 @@ import { MovimientosService }   from './movimiento/shared/movimientos.service';
 import { MovimientoEditorComponent }   from './movimiento/editor/movimiento-editor.component';
 import { MovimientoListaComponent }   from './movimiento/lista/movimiento-lista.component';
 import { MovimientoBalanceComponent }   from './movimiento/balance/movimiento-balance.component';
-/** 2. registro del módulo de comunicaciones */
+/** 
+ * 2. registro del módulo de comunicaciones
+ * */
 @NgModule({
   imports: [CommonModule, FormsModule, RouterModule, HttpModule],
   exports: [MovimientoComponent],
