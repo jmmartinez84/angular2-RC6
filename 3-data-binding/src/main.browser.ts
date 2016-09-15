ac35912c5af86771a1bd46a63c609baa031cbb5e
Alberto Basalo <albertobasalo@agorabinaria.com>
2016-09-15 17:46:24 +0200

    app2

--- 3-data-binding/src/main.browser.ts
+++ 3-data-binding/src/main.browser.ts
@@ -1,6 +1,29 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-
+import { NgModule } from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import { Component, OnInit } from '@angular/core';
 import {AppModule} from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+@Component({
+  selector: 'app-dos',
+  template: '<p>Funciona</p>'
+})
+export class App2Component implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
+
+@NgModule({
+  imports: [ BrowserModule],
+  declarations: [App2Component],
+  providers: [/* TODO: Providers go here */],
+  bootstrap: [App2Component],
+})
+export class App2Module { }
+
+platformBrowserDynamic().bootstrapModule(App2Module)
   .catch(err => console.error(err));
